e8b267ed64aad13c57224bc143d2504fd846bd55
Pranjal-png <pranjaldalal2004@gmail.com>
2026-07-06 16:16:22 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (38.24%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    void generate(vector<int>& nums, vector<vector<int>>&ans,vector<int>&temp,int index){
+        if(index == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[index]);
+        generate(nums,ans,temp,index+1);
+        temp.pop_back();
+        generate(nums,ans,temp,index+1);
+
+    }
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+
+        generate(nums,ans,temp,0);
+        return ans;
+        
+    }
+};
